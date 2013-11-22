5478067d43fb82ee57e9e4be56c67a55a605b7db
kazuto <kazuto@git.local.nanofi.net>
2013-11-22 11:25:32 +0900

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Gitlatex::Application.config.secret_key_base = '3b20e8a872c97982e9d47aa50ba2530018b56e28f97a95b92d146b367e9efca653f066f00dcb9facd0ea5de224f9082b14fcc95850090e8238ff04a8cbd3c7b2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Gitlatex::Application.config.session_store :cookie_store, key: '_gitlatex_session'
