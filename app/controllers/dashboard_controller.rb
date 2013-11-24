413d73906ff5d562419b3d5d6f05113b4213c0d2
nanofi <nanogenomu@gmail.com>
2013-11-24 14:46:01 +0900

    Dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,5 +2,6 @@ class DashboardController < ApplicationController
   before_action :authenticated!
   
   def index
+    @projects = ProjectDecorator.decorate_collection(Project.list || [])
   end
 end
