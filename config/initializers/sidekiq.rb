863d81ecbac97434e14ed624f30e4dbdb2013801
nanofi <nanogenomu@gmail.com>
2013-11-30 05:18:06 +0900

    Added sidekiq client setting

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,3 +1,6 @@
 Sidekiq.configure_server do |config|
   config.redis = { url: "redis://#{Settings.redis.host}:#{Settings.redis.port}", namespace: 'gitlatex' }
 end
+Sidekiq.configure_client do |config|
+  config.redis = { url: "redis://#{Settings.redis.host}:#{Settings.redis.port}", namespace: 'gitlatex' }
+end
