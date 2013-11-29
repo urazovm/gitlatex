0d1de772b5ee1099847c3a7d545677a537516c2a
nanofi <nanogenomu@gmail.com>
2013-11-30 08:35:41 +0900

    View branch name

--- app/decorators/build_decorator.rb
+++ app/decorators/build_decorator.rb
@@ -7,6 +7,8 @@ class BuildDecorator < Draper::Decorator
   end
   def message(status)
     h.capture do
+      h.concat h.content_tag(:small, h.t("builds.build.branch", branch: object.ref.split('refs/heads/')[1]))
+      h.concat h.tag(:br)
       h.concat h.content_tag(:small, h.t("builds.build.commit_id", id: object.commit_id))
       h.concat h.content_tag(:pre, object.commit_message)
     end
