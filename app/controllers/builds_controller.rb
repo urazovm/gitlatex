b84a55b53fbfbec62b308d452d8efd7f4367cd1f
nanofi <nanogenomu@gmail.com>
2014-04-26 23:30:32 +0000

    Style

--- app/controllers/builds_controller.rb
+++ app/controllers/builds_controller.rb
@@ -2,7 +2,7 @@ class BuildsController < AuthenticateController
   drop_breadcrumb I18n.t('crumb.projects'), projects_path
   
   def show
-    @project = Project.get(params[:project_id])
+    @project = Project.where(id: params[:project_id]).first
     @build = Build.where(id: params[:id]).first
     drop_breadcrumb @project.name_with_namespace, project_path(@project)
     drop_breadcrumb @build.commit_id, project_build_path(@project, @build)
